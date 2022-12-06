30cb892dfe160f70dc361df4a075f3c1daa41959
Baraa Hassan <baraa.alaa.eldin@gmail.com>
2022-12-06 03:28:26 +0000

    add the implementation to the repo

--- data/process_data.py
+++ data/process_data.py
@@ -1,22 +1,87 @@
 import sys
-
+from sqlalchemy import create_engine
+import pandas as pd
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    Load the data from disaster_messages.csv and disaster_categories.csv, besides merging them.
+
+            Parameters:
+                    messages_filepath (str): path to disaster_messages.csv
+                    categories_filepath (str): path to disaster_categories.csv
+
+            Returns:
+                    return the merged data
+    '''
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    # merge datasets
+    df = messages.merge(categories, on = 'id')
+    
+    # create a dataframe of the 36 individual category columns
+    categories = categories["categories"].str.split(";",expand=True)
+    
+    # select the first row of the categories dataframe
+    row = categories.loc[0]
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda entry: entry[:-2])
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype("str").str[-1]
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype("str").astype(int)
+    
+    # drop the original categories column from `df`
+    df.drop(["categories"], axis=1, inplace=True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    clean the dataframe from the duplicates.
 
+            Parameters:
+                    df (Dataframe): merged data
+
+            Returns:
+                    return the clean dataframe
+    '''
+    # drop duplicates
+    df.drop(df[df.duplicated()].index, inplace=True)
+    return df
+    
+    
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    Save the data to database.
+
+            Parameters:
+                    df (Dataframe): The dataframe to be saved to database.
+                    database_filename (str): the name of the database.
+                    
+    '''
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('InsertTableName', engine, index=False)
 
 
 def main():
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+        print(database_filepath)
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
@@ -25,6 +90,7 @@ def main():
         print('Cleaning data...')
         df = clean_data(df)
         
+        print(df)
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
         
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,152 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+import pickle
+import re
+import string
+# import libraries
+import pandas as pd
+import numpy as np
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sqlalchemy import create_engine
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics import classification_report
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
 
 def load_data(database_filepath):
-    pass
+    '''
+    Load the data from databaset.
 
+            Parameters:
+                    database file path (str): path to the databaset
+
+            Returns:
+                    return training data (X), label (Y), and the categories names
+    '''
+    # load data from database
+    engine = create_engine('sqlite:///data/YourDatabaseName.db')
+    df = pd.read_sql_table('InsertTableName', engine)
+
+    remove_indx = np.array([])
+    
+    # Removing messages that contain only spaces!
+    _,indeces = np.unique(df.message.values,return_inverse=True)
+    indeces_of_the_empty_messages = np.where((indeces==0)|(indeces==1)|(indeces==2))[0]
+    remove_indx = np.append(remove_indx,indeces_of_the_empty_messages)
+
+    # Removing nan catogries
+    for col_ind in range(4,40):
+        remove_indx = np.append(remove_indx,df[df[df.columns[col_ind]].isna()].index.values)
+
+    df.drop(np.unique(remove_indx), inplace=True)
+
+    X = df.message.values
+    Y = df[df.columns[4:]].values
+    
+    return X, Y, df.columns[4:]
 
 def tokenize(text):
-    pass
+    '''
+    tokenize the messages.
+
+            Parameters:
+                    text (str): message
+
+            Returns:
+                    return clean tokens from the input message
+    '''
+    # find URLs
+    detected_urls = re.findall(url_regex, text)
+    
+    # Replace the URLs with placeholder
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    
+    # Remove numbers from the text
+    text = re.sub(r'\d+', '', text)
+    
+    # Remove panctuation
+    translator = str.maketrans('', '', string.punctuation)
+    text = text.translate(translator)
+    
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    # clean tokens of the messages
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok.lower().strip())
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    '''
+    build the model.
+
+            Returns:
+                    The model pipeline.
+    '''
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    return pipeline 
+    
+    
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    build the model.
+            
+            Parameters:
+                    model (sklearn model): the trained model
+                    X_test (numpy array): test input data
+                    Y_test (numpy array): test label data
+                    category_names (list of strings): list of the categories names
+            
+            Returns:
+                    The model pipeline.
+    '''
+    Y_pred = pipeline.predict(X_test)
+    
+    for i in range(36):
+        print("report of the category",category_names[i],classification_report(Y_test[:,i], Y_pred[:,i]))
+    
+    
+
+
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    build the model.
+            
+            Parameters:
+                    model (sklearn model): the trained model
+                    model_filepath (str): the path where the model will be saved
+            
+            Returns:
+                    The model pipeline.
+    '''
+    
+    with open(model_filepath, "wb") as f:
+        pickle.dump(model, f)
 
 
 def main():
